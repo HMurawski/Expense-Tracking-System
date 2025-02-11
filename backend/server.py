150c9b490667ff2f8726e71365d358dad5cf8f1f
Hubert Murawski <h.murawski9@gmail.com>
2025-02-11 17:06:25 +0100

    added analytics post method

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import date
 import db_helper
 from typing import List
 from pydantic import BaseModel
 
+app = FastAPI()
 class Expense(BaseModel):
     
     amount: float
     category: str
     notes: str
 
+class DateRange(BaseModel):
+    start_date: date
+    end_date: date
+    
 
-app = FastAPI()
+
+
 @app.get("/expenses/{expense_date}", response_model=List[Expense])
 def get_expenses(expense_date: date):
     expenses = db_helper.fetch_expenses_for_date(expense_date)
@@ -25,3 +31,10 @@ def add_or_update_expense(expense_date: date, expenses:List[Expense]):
         db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
         
     return {"message": "Expenses updated successfully"}
+
+@app.post("/analytics/")
+def get_analytics(date_range: DateRange):
+    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
+    if data is None:
+        raise HTTPException(status_code=500, detail="Failed to retrieve expense summary from the database")
+    return data
